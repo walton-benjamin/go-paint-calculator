e697ea6b3777cd1e0b82910a78da2086187e7fa5
Benjamin Walton <psybw4@nottingham.ac.uk>
2023-01-26 15:55:29 +0000

    getQuantity & Extras slice added

--- BackEnd/queryExtras.go
+++ BackEnd/queryExtras.go
@@ -10,13 +10,19 @@ import (
 func QueryExtras() [][]string {
 
 	fmt.Printf("\n\n Would you like any extras added to your order? Items in stock : ")
+	extrasOrdered := {{"0"}}
 	extras := [][]string{
-		{"Paintbrush 2 inch", "2", "3.99"},
-		{"Paint tray", "1", "1.99"},
+		{"2 inch bristles Paintbrush", "3.99"},
+		{"4 inch bristles Paintbrush", "5.99"},
+		{"Paint Roller 6 inch brush", "5.99"},
+		{"Paint Roller Long Handle", "8.99"},
+		{"Paint Roller Short Handle", "4.99"},
+		{"Paint tray", "1.99"},
+		{"Overall", "10.00"},
 	}
 
-	for _, thisSlice := range extras {
-		fmt.Printf(" 1. %s , £%s per item \n", thisSlice[0], thisSlice[2])
+	for i, thisSlice := range extras {
+		fmt.Printf(" %d. %s , £%s per item \n", i+1 , thisSlice[0], thisSlice[2])
 	}
 
 	moreOrders := true
@@ -31,7 +37,14 @@ func QueryExtras() [][]string {
 			fmt.Println("If this was a mistake, type 'x', 'c' or 'cancel'.")
 			fmt.Println("Which item would you like to order? : ")
 			scanner.Scan()
-			thisItem := scanner.Text()
+			itemInString := scanner.Text()
+			//check if item is found in the available products
+			if strings.Contains(itemInString, "#"){
+				
+			}else{
+
+			}
+
 
 			//TC Which item
 			quantity := getQuantity(thisItem)
@@ -45,13 +58,23 @@ func QueryExtras() [][]string {
 		fmt.Printf("")
 	}
 
-	return [][]string{{"0"}}
+	return extrasOrdered
 }
 
 func getQuantity(thisItem string) int {
 	validInput := false
+	var numberToOrder int
 	for validInput {
-		fmt.Printf("How many '%s' would you like to order? : ")
+		fmt.Printf("How many '%ss' would you like to order? : ")
+		scanner.Scan()
+		inString := scanner.Text()
+		numberOrdered, err := stsrings.ParseInt(inString,10, 64)
+		if err != nil{
+			fmt.Println("Sorry, that wasn't a valid number to order. Try again...")
+		} else {
+			numberToOrder = numberOrdered
+			validInput = true
+		}
 	}
-	return 0
+	return numberOrdered
 }
